210b23e70bfd944aa70b99e01d2137e631ef2408
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-11-15 23:37:46 +0500

    fgsfdsfds

--- get_count_users.py
+++ get_count_users.py
@@ -1,5 +1,6 @@
-from webbrowser import get
-import get_data
+
+
+import json
 
 def get_count_users(data:dict) -> int:
     """
@@ -10,4 +11,15 @@ def get_count_users(data:dict) -> int:
     Returns:
         int: number of users
     """
+    d=open(data,  'r', encoding='utf8').read()
+    dd=json.loads(d)
+
+    return len(dd['results'])
+
+print(get_count_users('aaa.json'))
+
+
+
+
+
     
